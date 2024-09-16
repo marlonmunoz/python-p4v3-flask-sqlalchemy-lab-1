483430212cf7eb36f14dadda232d0b180aa290c3
Marlon Munoz <marlon@marlonshade.com>
2024-09-16 14:56:54 -0400

    lab-1

--- server/app.py
+++ server/app.py
@@ -21,6 +21,34 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = {'id': earthquake.id,
+                'location': earthquake.location,
+                'magnitude': earthquake.magnitude,
+                'year': earthquake.year}
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    eqs = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    result = []
+    for eq in eqs:
+        result.append(eq.to_dict())
+
+    return {
+        'count': len(eqs),
+        'quakes': [eq.to_dict() for eq in eqs]
+    }
 
 
 if __name__ == '__main__':
